9afde977b93b1600324464c32dd625a1cc5de5d0
vivianatran <tranvivianaedu@gmail.com>
2023-09-30 16:52:02 -0700

    meh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react'; 
 import './App.css';
+import bioehsc from './pages/BioEHSC';
+import committees from './pages/committees';
+import courseMap from './pages/coursemap';
+import decal from './pages/decal';
+import home from './pages/home';
+import leadership from './pages/leadership';
+
+
+import 
 function App() {
-    return <div className='App'/>
+    return <div className="App">
+    <home />
+    </div>
   }
