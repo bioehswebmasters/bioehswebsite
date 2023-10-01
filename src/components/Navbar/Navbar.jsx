8ca3d448be1ca64865d6dbf0f8969366988ac2be
vivianatran <tranvivianaedu@gmail.com>
2023-09-30 20:34:54 -0700

    navbar styling in but it is not showing the dropdown menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import CommitteesDropdown from '../Website Functions/CommitteesDropdown';
-import logo from '../../Images/UniversalClubPictures/BioEHS_ModernLogo.png'; // Adjust the path
+import React from 'react'
+import { Link } from 'react-router-dom'
+import CommitteesDropdown from '../Website Functions/CommitteesDropdown'
+import './Navbar.css'; 
 
 function Navbar() {
   return (
     <nav className="navbar">
-    <div className="logo">
-      <img src={logo} alt="Logo" />
-    </div>
-    <div className="options">
-      <Link to="/BioEHSC">BioEHSC</Link>
-      <CommitteesDropdown />
-      <Link to="/CourseMap">Course Map</Link>
-      <Link to="/Decal">Decal</Link>
-      <Link to="/Leadership">Leadership</Link>
-    </div>
-  </nav>
-  );
+      <div className="options">
+        <Link to="/BioEHSC">BioEHSC</Link>
+        <CommitteesDropdown />
+        <Link to="/CourseMap">Course Map</Link>
+        <Link to="/Decal">Decal</Link>
+        <Link to="/Leadership">Leadership</Link>
+      </div>
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
