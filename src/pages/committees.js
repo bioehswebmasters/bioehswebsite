6fff11040d77b6003daaefc39d73eba51d65ddd6
vivianatran <tranvivianaedu@gmail.com>
2023-10-08 14:58:00 -0700

    committees dropdown in weird location

--- src/pages/committees.js
+++ src/pages/committees.js
@@ -1,11 +1,13 @@
 import React from "react";
-//import {CommitteesDropdown} from '/../components/Website Functions';
+//import CommitteesDropdown from 'src/components/WebsiteFunctions';
+import CommitteesDropdown from "../components/WebsiteFunctions/CommitteesDropdown";
 //put candidate information as well here
 //<CommitteesDropdown/>
 function Committees(){
     return(
         <div>
         <h1> Welcome to committees</h1>
+        <CommitteesDropdown/>
         </div>
     )
 }
