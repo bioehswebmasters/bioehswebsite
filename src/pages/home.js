3d8fa1d886de5453a5571a5c5f43172cc3514803
vivianatran <tranvivianaedu@gmail.com>
2023-11-05 17:19:00 -0800

    added join us button (no click stuff yet)

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,16 +7,10 @@ function Home() {
     <div className="img-container">
     <div className="textOverLayed">
           <h1>Bioengineering Honor Society</h1>
-          <p>
-
-Recognize outstanding UC Berkeley Bioengineering undergraduates for their academic
-
- excellence. Alongside strong scholastic achievements, the society promotes exceptional 
-
-character and  encourages members to engage in volunteer work, professional 
-
-development, and community events.
-</p>
+          <p>Recognize outstanding UC Berkeley Bioengineering undergraduates for their academic excellence. Alongside strong 
+            scholastic achievements, the society promotes exceptional character and encourages members to engage in volunteer work, professional development, and community events.
+            </p>
+            <button className="join-button">Join Us</button>
         </div>
         <img src={homeClubImage} alt="Home" className="coverimage" />
         </div>
