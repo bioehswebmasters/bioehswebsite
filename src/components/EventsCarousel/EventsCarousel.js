398b2b6e8107d303b9fdb085ca717501d3ee5f7c
vivianatran <tranvivianaedu@gmail.com>
2023-11-09 09:24:07 -0800

    existing carousel but not in the middle

--- src/components/EventsCarousel/EventsCarousel.js
+++ src/components/EventsCarousel/EventsCarousel.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import getImagesFromFolder from './GetImages';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './EventsCarousel.css'; // Add this import
 
 const EventsCarousel = ({ eventFolder }) => {
     const images = getImagesFromFolder(eventFolder);
 
     return (
-        <Carousel style={{ maxWidth: '800px', maxHeight: '800px'}}>
+        <Carousel className="custom-carousel">
             {images.map((image, index) => (
                 <Carousel.Item key={index}>
-                    <img src={require(`../../Images/Events/Fall2023/October2023/${eventFolder}/${image}`)} height='300'
-                    width='400' alt={`Slide ${index + 1}`} />
+                    <div className="custom-image-container">
+                        <img
+                            src={require(`../../Images/Events/Fall2023/October2023/${eventFolder}/${image}`)}
+                            alt={`Slide ${index + 1}`}
+                            className="custom-image"
+                        />
+                    </div>
                     <Carousel.Caption>
                         <h3>Slide {index + 1}</h3>
                     </Carousel.Caption>
