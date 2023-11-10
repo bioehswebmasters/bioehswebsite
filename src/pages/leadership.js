9cc76088b92a969382426ae8e7464858bd0b17a4
vivianatran <tranvivianaedu@gmail.com>
2023-11-09 23:48:15 -0800

    leadership page

--- src/pages/leadership.js
+++ src/pages/leadership.js
@@ -1,13 +1,45 @@
 import React from "react";
-
+import OfficerCard from "../components/Officer Card/Card";
+import './Leadership.css';
 
 function Leadership() {
-  return (
-    <div className="leadership-container">
-          <section id="executive-board">
-        {/* Content for Executive Board */}
-      </section>
+  // Sample officer data 
+  const officers = [
+    { name: "Officer 1", position: "President" },
+    { name: "Officer 2", position: "Vice President" },
+    { name: "Officer 3", position: "Treasurer" },
+    // ... add more officers
+  ];
 
+  // Function to group officers into rows
+  const groupOfficersIntoRows = (officers, officersPerRow) => {
+    const rows = [];
+    for (let i = 0; i < officers.length; i += officersPerRow) {
+      rows.push(officers.slice(i, i + officersPerRow));
+    }
+    return rows;
+  };
+
+  // Set the maximum number of officers per row
+  const officersPerRow = 3;
+
+  // Group officers into rows
+  const officerRows = groupOfficersIntoRows(officers, officersPerRow);
+
+  return (
+    <div className="body">
+      <div className="leadership-container">
+        <section id="executive-board">
+          <h1>Executive Board</h1>
+          {/* Content for Executive Board */}
+          {officerRows.map((row, rowIndex) => (
+            <div key={rowIndex} className="officer-cards-container">
+              {row.map((officer, index) => (
+                <OfficerCard key={index} name={officer.name} position={officer.position} />
+              ))}
+            </div>
+          ))}
+        </section>
       <section id="nonexecutive-board">
         {/* Content for Non-Executive Board */}
       </section>
@@ -20,7 +52,7 @@ function Leadership() {
         {/* Content for Senior Advisers */}
       </section>
     </div>
-
+    </div>
   );
 }
 
